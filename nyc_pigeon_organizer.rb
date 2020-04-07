c1dda0a5ca1687c424a9f0fe3a94f6c4d133665e
Daniel Andrew <dwandrew@hotmail.com>
2020-04-07 12:32:48 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -29,7 +29,9 @@ pigeon_data[:color].each {|k, v| pigeon_names.push(v)}
 pigeon_names.flatten!
 pigeon_names.uniq!
 pigeon_names.each{|name| pigeons[name]={:color =>[], :gender =>[], :lives =>[]}}
-pigeon_data.each {|trait, attribute|p trait; p attribute}
+pigeon_data.each {|trait, attribute| trait;  attribute
+  attribute.each {|top, name| p top; p name}
+}
 
 pigeons
 end
